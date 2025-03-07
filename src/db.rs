8c2fa1f258e31f961d1576b03f667f2161b2e499
Rifai <84585203+rifainareswara@users.noreply.github.com>
2025-03-07 23:02:38 +0800

    Fix and New Feature
    - Change name database
    - Adding Cargo Watch

--- src/db.rs
+++ src/db.rs
@@ -2,5 +2,5 @@ use mongodb::{Client, Database};
 
 pub async fn connect_db(uri: &str) -> mongodb::error::Result<Database> {
     let client = Client::with_uri_str(uri).await?;
-    Ok(client.database("test"))
+    Ok(client.database("daftar_mahasiswa"))
 }
